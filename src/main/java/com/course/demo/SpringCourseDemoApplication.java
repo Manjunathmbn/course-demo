e7455986099016d28b34691ec27124a23edd83fb
Manjunathmbn <44604424+Manjunathmbn@users.noreply.github.com>
2025-05-31 10:19:12 +0530

    Update SpringCourseDemoApplication.java

--- src/main/java/com/course/demo/SpringCourseDemoApplication.java
+++ src/main/java/com/course/demo/SpringCourseDemoApplication.java
@@ -1,20 +1,20 @@
-//package com.course.demo;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@SpringBootApplication
-//@RestController
-//public class SpringCourseDemoApplication {
-//
-//	@GetMapping("/message")
-//	public String message(){
-//		return "Welcome to the Spring Course Demo App!";
-//	}
-//	public static void main(String[] args) {
-//		SpringApplication.run(SpringCourseDemoApplication.class, args);
-//	}
-//
-//}
+package com.course.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCourseDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "Welcome to the Spring Course Demo App!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseDemoApplication.class, args);
+	}
+
+}
